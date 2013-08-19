4aa0f13cf9eefc7a83707b1191276d3826f193b7
Ivan Habunek <ivan.habunek@gmail.com>
2013-08-19 10:50:03 +0200

    Fixed bug in tests

--- tests/RandomTest.php
+++ tests/RandomTest.php
@@ -4,7 +4,7 @@ class RandomTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetRandomNumber()
     {
-        $rng = new RandomNumberGenerator();
+        $rng = new Random();
         $actual = $rng->getRandomNumber();
         $this->assertSame(4, $actual);
     }
